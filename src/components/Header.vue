dd25a1f0339f3fe41b61f0ecdaee002ee46e4889
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2020-10-22 17:45:50 -0700

    set up Header and Home pages, remove unnecessary vuex files, add some basic less and images in public/

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,17 @@
 <template>
-  <nav class="nav">
-    <router-link :to="'/'">Home</router-link>
-  </nav>
+  <header>
+    <nav class="navigation">
+      <router-link to="/">Home</router-link>
+      <router-link to="/projects">Projects</router-link>
+      <!-- <router-link to="/blog">Blog</router-link> -->
+      <a
+        target="_blank"
+        href="https://drive.google.com/open?id=1lPcfiK6UE7d8oni8ZjtaOvahiPdBnU8K"
+        >Resume</a
+      >
+      <router-link to="/contact">Contact</router-link>
+    </nav>
+  </header>
 </template>
 
 <script>
@@ -9,16 +19,29 @@ export default {}
 </script>
 
 <style lang="less" scoped>
-.nav {
-  display: flex;
-  background: cyan;
+@import '../_settings.less';
 
+header {
+  .flexbox-property();
+  padding: 20px 0;
   a {
-    padding: 10px 5px;
     text-decoration: none;
-
-    :hover {
-      background: lightpink;
+    color: @font-color-main;
+    padding: 20px 15px;
+    margin: 10 15px;
+  }
+  a:hover {
+    .hover();
+  }
+  @media only screen and (max-width: 360px) {
+    margin: 0px;
+    nav {
+      display: flex;
+      flex-direction: column-reverse;
+      a {
+        padding: 5px 20px;
+        border-bottom: 1px solid darkgray;
+      }
     }
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import Vue from 'vue'
 
 import App from './App.vue'
 import router from './utils/routes'
-import store from './utils/store/store'
 
 import './index.css'
 
 new Vue({
   router,
-  store,
   render: (h) => h(App),
 }).$mount('#root')
