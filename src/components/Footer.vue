0745ae3d92f26225eca7d29cb254b336508d73b4
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2020-10-23 12:25:09 -0700

    add Contact, Footer, and Projects components, update routes, add less stylings

---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer>
+    <p>Copyright Jeffrey David Whitaker 2019</p>
+  </footer>
+</template>
+
+<style lang="less" scoped>
+footer {
+  text-align: center;
+  margin-bottom: 10px;
+}
+</style>
--- src/utils/routes.js
+++ src/utils/routes.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import Home from '../components/Home.vue'
+import Contact from '../components/Contact.vue'
+import Projects from '../components/Projects.vue'
 
 Vue.use(VueRouter)
 
@@ -10,6 +12,16 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: Contact,
+  },
+  {
+    path: '/projects',
+    name: 'Projects',
+    component: Projects,
   },
 ]
 
